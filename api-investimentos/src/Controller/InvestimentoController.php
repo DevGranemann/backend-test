26c17e71a2aa73c383acf83f49f823a3fe71aedf
DevGranemann <ribeiroeduardogranemann@gmail.com>
2025-09-03 22:34:44 -0300

    Controller created for a investment cadastration

--- api-investimentos/src/Controller/InvestimentoController.php
+++ api-investimentos/src/Controller/InvestimentoController.php
@@ -2,17 +2,41 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Investment;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\Request;
 class InvestimentoController extends AbstractController {
-    #[Route('/api/investimentos', name: 'listar_investimentos', methods: ['GET'])]
-    public function index(): JsonResponse {
+    #[Route('/api/investments', name: 'create_investments', methods: ['POST'])]
+    public function create(Request $request, EntityManagerInterface $em): JsonResponse {
+
+        $data = json_decode($request->getContent(), true);
+
+        if (!$data || !isset($data['owner'], $data['creationDate'], $data['investmentValue'])) {
+
+            return $this->json([
+                'error' => 'Parâmetros inválidos. Informe: nome do proprietário (owner), a data da criação do investimento (creationDate) e o valor do investimento (investmentValue).'
+            ], 400);
+        }
+
+        $investment = new Investment();
+        $investment->setOwner($data['owner']);
+        $investment->setCreationDate(new \DateTime($data['creationDate']));
+        $investment->setInvestmentValue((float) $data['investmentValue']);
+
+        $em->persist($investment);
+        $em->flush();
 
         return $this->json([
-            'message' => 'Bem-vindo à API de Investimentos',
-            'status' => 'okay',
-        ]);
+            'message' => 'Investimento criado com sucesso.',
+            'Investment' => [
+                'id' => $investment->getId(),
+                'ownerName' => $investment->getOwner(),
+                'creationDate' => $investment->getCreationDate()->format('Y-m-d H:i:s'),
+                'investmentValue' => $investment->getInvestmentValue(),
+            ]
+        ], 201);
     }
 }
