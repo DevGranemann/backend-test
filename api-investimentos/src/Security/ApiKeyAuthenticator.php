e1c38057b6d415f1abc7a352440e35a7e3063486
DevGranemann <ribeiroeduardogranemann@gmail.com>
2025-09-09 03:37:31 -0300

    Configured default file for us API

--- api-investimentos/src/Security/ApiKeyAuthenticator.php
+++ api-investimentos/src/Security/ApiKeyAuthenticator.php
@@ -2,43 +2,52 @@
 
 namespace App\Security;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 
 class ApiKeyAuthenticator extends AbstractAuthenticator
 {
     public function supports(Request $request): ?bool
     {
-        // TODO: Implement supports() method.
+        return $request->headers->has('X-API-KEY');
     }
 
     public function authenticate(Request $request): Passport
     {
-        // TODO: Implement authenticate() method.
+        $apiKey = $request->headers->get('X-API-KEY');
+
+        if ($apiKey !== $_ENV['API_KEY']) {
+            throw new AuthenticationException('Chave da API inválida');
+        }
+
+        return new Passport(
+            new UserBadge('api-user'),
+            new CustomCredentials(
+                function ($credentials, $user) {
+                    return $credentials === $_ENV['API_KEY'];
+                },
+                $apiKey
+            )
+        );
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        // TODO: Implement onAuthenticationSuccess() method.
+        return null;
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        // TODO: Implement onAuthenticationFailure() method.
+        return new JsonResponse([
+            'error' => 'Acesso negado: chave inválida'
+        ], 401);
     }
 
-    //    public function start(Request $request, ?AuthenticationException $authException = null): Response
-    //    {
-    //        /*
-    //         * If you would like this class to control what happens when an anonymous user accesses a
-    //         * protected page (e.g. redirect to /login), uncomment this method and make this class
-    //         * implement Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface.
-    //         *
-    //         * For more details, see https://symfony.com/doc/current/security/experimental_authenticators.html#configuring-the-authentication-entry-point
-    //         */
-    //    }
 }
